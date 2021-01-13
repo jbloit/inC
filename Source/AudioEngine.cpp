2c6b63ef3454bef00d7f3aefda84666aca317a1b
Julien Bloit <julien.bloit@gmail.com>
2021-01-13 15:06:02 +0100

    hooked link to audio engine. prints out current tempo.

--- Source/AudioEngine.cpp
+++ Source/AudioEngine.cpp
@@ -9,9 +9,94 @@
 */
 
 #include "AudioEngine.h"
-AudioEngine::AudioEngine(){}
-AudioEngine::~AudioEngine(){}
+AudioEngine::AudioEngine()
+{
+    initLink();
+}
+AudioEngine::~AudioEngine()
+{
+    if (link->isEnabled())
+        link->enable(false);
+}
+
+#pragma mark - AudioSource
 
 void AudioEngine::prepareToPlay (int samplesPerBlockExpected, double sampleRate) {}
 void AudioEngine::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) {}
 void AudioEngine::releaseResources() {}
+
+
+#pragma mark - Link
+
+void AudioEngine::initLink()
+{
+    link.reset(new ableton::Link{ 60 });
+    link->setTempoCallback([this](const double p) { DBG("TEMPO CHANGED TO " << juce::String(p)); });
+    link->enable(true);
+}
+
+void AudioEngine::calculate_output_time(const double sample_rate, const int buffer_size)
+{
+    // Synchronize host time to reference the point when its output reaches the speaker.
+    const auto host_time =  host_time_filter.sampleTimeToHostTime(sample_time);
+    const auto output_latency = std::chrono::microseconds{ std::llround(1.0e6 * buffer_size / sample_rate) };
+    output_time = output_latency + host_time;
+}
+
+
+
+std::chrono::microseconds AudioEngine::calculateTimeAtSample(const std::uint64_t sampleTime, const double sample_rate, const int buffer_size)
+{
+    const auto host_time =  host_time_filter.sampleTimeToHostTime(sampleTime);
+    const auto output_latency = std::chrono::microseconds{ std::llround(1.0e6 * buffer_size / sample_rate) };
+    return output_latency + host_time;
+}
+
+
+AudioEngine::EngineData AudioEngine::pull_engine_data()
+{   // Safely operate on data isolated from user changes.
+    auto engine_data = EngineData{};
+    if (engine_data_guard.try_lock()) {
+        engine_data.requested_bpm = shared_engine_data.requested_bpm;
+        shared_engine_data.requested_bpm = 0;
+        
+        engine_data.request_start = shared_engine_data.request_start;
+        shared_engine_data.request_start = false;
+        
+        engine_data.request_stop = shared_engine_data.request_stop;
+        shared_engine_data.request_stop = false;
+        
+        lock_free_engine_data.quantum = shared_engine_data.quantum;
+        lock_free_engine_data.startstop_sync = shared_engine_data.startstop_sync;
+        
+        engine_data_guard.unlock();
+    }
+    else
+        DBG("entry failed");
+    engine_data.quantum = lock_free_engine_data.quantum;
+    return engine_data;
+}
+
+
+void AudioEngine::process_session_state(const EngineData& engine_data)
+{
+    session = std::make_unique<ableton::Link::SessionState>(link->captureAudioSessionState());
+
+    if (engine_data.request_start)
+        session->setIsPlaying(true, output_time);
+
+    if (engine_data.request_stop)
+        session->setIsPlaying(false, output_time);
+
+    if (!is_playing && session->isPlaying()) {   // Reset the timeline so that beat 0 corresponds to the time when transport starts
+        session->requestBeatAtTime(0., output_time, engine_data.quantum);
+        is_playing = true;
+    }
+    else if (is_playing && !session->isPlaying())
+        is_playing = false;
+
+    if (engine_data.requested_bpm > 0) // Set the newly requested tempo from the beginning of this buffer.
+        session->setTempo(engine_data.requested_bpm, output_time);
+
+    link->commitAudioSessionState(*session); // Timeline modifications are complete, commit the results
+}
--- Source/AudioEngine.h
+++ Source/AudioEngine.h
@@ -10,14 +10,50 @@
 
 #pragma once
 #include <JuceHeader.h>
+
+#include <ableton/Link.hpp>
+#include <ableton/link/HostTimeFilter.hpp>
+
 class AudioEngine : public juce::AudioSource
 {
 public:
     AudioEngine();
     ~AudioEngine();
     
-    // AudioSource
+#pragma mark - AudioSource
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
+    
+#pragma mark - Link
+    
+    /** Data that's passed around between thread */
+    struct EngineData
+    {
+        double requested_bpm;
+        bool request_start;
+        bool request_stop;
+        double quantum;
+        bool startstop_sync;
+        JUCE_LEAK_DETECTOR(EngineData)
+    };
+    
+    void initLink();
+    
+    void calculate_output_time(const double sample_rate, const int buffer_size);
+    std::chrono::microseconds calculateTimeAtSample(const std::uint64_t sampleTime, const double sample_rate, const int buffer_size);
+    EngineData pull_engine_data();
+    void process_session_state(const EngineData& engine_data);
+    
+    std::unique_ptr<ableton::Link> link;
+    ableton::link::HostTimeFilter<ableton::link::platform::Clock> host_time_filter;
+    std::unique_ptr<ableton::Link::SessionState> session;
+    
+    EngineData shared_engine_data, lock_free_engine_data;
+    std::mutex engine_data_guard;
+    
+    std::chrono::microseconds output_time;
+    std::uint64_t sample_time = 0;
+    bool is_playing = false;
+    
 };
